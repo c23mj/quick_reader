229511d0aed071db6e00b562bad07effd9bb8de7
Michael Jiang <c23mj@dalton.org>
2023-04-13 12:58:41 -0400

    messed with document.getElementbyId.

---
+++ assets/popup.js
@@ -0,0 +1,10 @@
+const activate = document.getElementById("br-activate");
+const reset = document.getElementById("br-reset");
+
+// activate.addEventListener("click", () => {
+
+// });
+
+// reset.addEventListener("click", () => {
+
+//});
---
+++ content_scripts.js
@@ -0,0 +1,4 @@
+var text = document.querySelectorAll("p", "span");
+for (var i = 0; i < text.length; i++) {
+  text[i].innerText = "test";
+}
