da4ebf9b02955520e8bdf51bbbfe417c4b206921
Michael Jiang <c23mj@dalton.org>
2023-04-16 17:03:41 -0400

    changed JSON file and continued refactoring stuff.

---
+++ background.js
@@ -0,0 +1,5 @@
+// $("#button").on("click", function () {
+//   chrome.tabs.getSelected(null, function (tab) {
+//     chrome.tabs.executeScript(tab.id, { file: "test.js" });
+//   });
+// });
--- content_scripts.js
+++
@@ -1,4 +0,0 @@
-var text = document.querySelectorAll("p", "span");
-for (var i = 0; i < text.length; i++) {
-  text[i].innerText = "test";
-}
